3c61975f6d65038bfc5a4b4a206ab1a8704ae1a5
student <lilkundiro@gmail.com>
2022-09-16 07:49:58 +0300

    tables in templates

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_account, register_card, register_currency, register_customer, register_loan, register_notification, register_receipt, register_reward, register_thirdparty, register_transaction, register_wallet
+from .views import list_account, list_card, list_customers, list_notification, list_thirdparty, list_transaction, register_account, register_card, register_currency, register_customer, register_loan, register_notification, register_receipt, register_reward, register_thirdparty, register_transaction, register_wallet,list_currency,list_wallet,list_account
 urlpatterns =[
     path("register/", register_customer, name="registration"),
     path("currency/", register_currency, name="registration"),
@@ -11,7 +11,18 @@ urlpatterns =[
     path("notification/", register_notification, name="registration"),
     path("receipt/", register_receipt, name="registration"),
     path("loan/", register_loan, name="registration"),
-    path("reward/", register_reward, name="registration")
+    path("reward/", register_reward, name="registration"),
+    path("customers/",list_customers,name="customers_list"),
+    path("currencies/",list_currency,name="currency_list"),
+    path("wallets/",list_wallet,name="wallet_list"),
+    path("accounts/",list_account,name="account_list"),
+    path("transactions/",list_transaction,name="transaction_list"),
+    path("cards/",list_card,name="card_list"),
+    path("thirdparties/",list_thirdparty,name="thirdparty_list"),
+    path("notifications/",list_notification,name="notification_list")
+   
+   
+   
    
    
    
--- app/views.py
+++ app/views.py
@@ -1,51 +1,145 @@
 from django.shortcuts import render
 
-from app.models import Notification
+
+from app.models import Account, Card, Currency, Customer, Notification, Thirdparty, Wallet, Transaction
 from .forms import AccountRegistrationForm, CardRegistrationForm, CurrencyRegistrationForm, CustomerRegistrationForm, LoanRegistrationForm, NotificationRegistrationForm, ReceiptRegistrationForm, RewardRegistrationForm, ThirdPartyRegistrationForm, TransactionRegistrationForm, WalletRegistrationForm
 
 # Create your views here.
 def register_customer(request):
-    form=CustomerRegistrationForm()
+    if request.method=="POST":
+        form=CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form=CustomerRegistrationForm()
     return render(request,"myproject/register_customer.html",{"form":form})
 
+
+
+
 def register_currency(request):
-    form=CurrencyRegistrationForm
-    return render(request,"myproject/register_currency.html",{"form":form})
+   if request.method=="POST":
+        form=CurrencyRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+   else:
+        form=CustomerRegistrationForm()
+   return render(request,"myproject/register_currency.html",{"form":form})
+
+
 
 def register_wallet(request):
-    form=WalletRegistrationForm
-    return render(request,"myproject/register_wallet.html",{"form":form})
+   if request.method=="POST":
+        form=WalletRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+   else:
+        form=WalletRegistrationForm()
+   return render(request,"myproject/register_wallet.html",{"form":form})
+
 
 def register_account(request):
-    form=AccountRegistrationForm
-    return render(request,"myproject/register_account.html",{"form":form})
+   if request.method=="POST":
+        form=AccountRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+   else:
+        form=AccountRegistrationForm()
+   return render(request,"myproject/register_account.html",{"form":form})
 
 def register_transaction(request):
-    form=TransactionRegistrationForm
-    return render(request,"myproject/register_transaction.html",{"form":form})
+     if request.method=="POST":
+        form=TransactionRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+     else:
+        form=TransactionRegistrationForm()
+     return render(request,"myproject/register_transaction.html",{"form":form})
 
 def register_card(request):
-    form=CardRegistrationForm
-    return render(request,"myproject/register_card.html",{"form":form})
+     if request.method=="POST":
+        form=CardRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+     else:
+        form=CardRegistrationForm()
+     return render(request,"myproject/register_card.html",{"form":form})
 
 def register_thirdparty(request):
-    form=ThirdPartyRegistrationForm
-    return render(request,"myproject/register_thirdyparty.html",{"form":form})
+  if request.method=="POST":
+        form=ThirdPartyRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+          form=ThirdPartyRegistrationForm()
+        return render(request,"myproject/register_thirdparty.html",{"form":form})
 
 def register_notification(request):
-    form=NotificationRegistrationForm
-    return render(request,"myproject/register_notification.html",{"form":form})
+  if request.method=="SEND":
+        form=NotificationRegistrationForm(request.SEND)
+        if form.is_valid():
+            form.save()
+        else:
+          form=NotificationRegistrationForm()
+        return render(request,"myproject/register_notification.html",{"form":form})
 
 def register_receipt(request):
-    form=ReceiptRegistrationForm
+  if request.method=="SEND":
+    form=ReceiptRegistrationForm(request.SEND)
+    if form.is_valid():
+            form.save()
+    else:
+          form=ReceiptRegistrationForm()
     return render(request,"myproject/register_receipt.html",{"form":form})
 
 def register_loan(request):
-    form=LoanRegistrationForm
+  if request.method=="SEND":
+    form=LoanRegistrationForm(request.SEND)
+    if form.is_valid():
+            form.save()
+    else:
+          form=LoanRegistrationForm()
     return render(request,"myproject/register_loan.html",{"form":form})
 
 def register_reward(request):
-    form=RewardRegistrationForm
+    if request.method=="SEND":
+      form=RewardRegistrationForm(request.SEND)
+      if form.is_valid():
+            form.save()
+    else:
+          form=RewardRegistrationForm()
     return render(request,"myproject/register_reward.html",{"form":form})
 
+def list_customers(request):
+    customers=Customer.objects.all()
+    return render(request,"myproject/customers_list.html",{"customers":customers})
+
+def list_currency(request):
+    currencies=Currency.objects.all()
+    return render(request,"myproject/currency_list.html",{"currencies":currencies})
+
+def list_wallet(request):
+    wallets=Wallet.objects.all()
+    return render(request,"myproject/wallet_list.html",{"wallets":wallets})
+
+def list_account(request):
+    accounts=Account.objects.all()
+    return render(request,"myproject/account_list.html",{"accounts":accounts})
+
+def list_transaction(request):
+    transactions=Transaction.objects.all()
+    return render(request,"myproject/transaction_list.html",{"transactions":transactions})
+
+def list_card(request):
+    cards=Card.objects.all()
+    return render(request,"myproject/card_list.html",{"cards":cards})
+
+def list_thirdparty(request):
+    thirdparties=Thirdparty.objects.all()
+    return render(request,"myproject/thirdparty_list.html",{"thirdparties":thirdparties})
+
+
+def list_notification(request):
+    notifications=Notification.objects.all()
+    return render(request,"myproject/notification_list.html",{"notifications":notifications})
 
